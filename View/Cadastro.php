c3ab846500d068595aa23bd5fe22c14281770f41
IanMaxAlmeidaLima <iancna360@gmail.com>
2025-07-26 19:01:04 -0300

    feat: Adicionado pag cadastro

--- View/Cadastro.php
+++ View/Cadastro.php
@@ -19,6 +19,7 @@
     <h2>Crie sua conta</h2>
     <p><strong>Preencha os campos para se cadastrar</strong></p>
 
+    //Formulário de Cadastro
     <label for="nome">Nome completo</label>
     <input type="text" id="nome" placeholder="Digite seu nome completo" />
 
